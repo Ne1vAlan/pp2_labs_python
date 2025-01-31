30bbd0531ca37ded1cc6b7ffcffec427bd43f763
Alan <alan.university2024@gmail.com>
2025-01-31 12:07:26 +0600

    Lab_3Fun1

--- Lab_3/Function1.py
+++ Lab_3/Function1.py
@@ -1,6 +1,6 @@
 def grams_to_ounces(grams):
     return 28.3495231 * grams
 
-grams = float(input("How many grams do you need?:"))
+grams = float(input())
 Ounce = grams_to_ounces(grams)
 print("Youre ounces:", Ounce)
