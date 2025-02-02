bd134f21beaea754b485663e8ff75064eda4babe
Alan <alan.university2024@gmail.com>
2025-02-02 22:27:28 +0600

    Lab_3Fun13.made spaces

--- Lab_3/Function13.py
+++ Lab_3/Function13.py
@@ -4,7 +4,7 @@ def guess_the_number():
     print("Hello! What is your name?")
     name = input()
 
-    print(f"Well,{name}, I am thinking of a number between 1 and 20.")
+    print(f"Well, {name}, I am thinking of a number between 1 and 20.")
 
     secret_number = random.randint(1, 20)
     attempts = 0
@@ -19,7 +19,7 @@ def guess_the_number():
         elif guess > secret_number:
             print("Your guess is too high.")
         else:
-            print(f"Good job,{name}! You guessed my number in {attempts} guesses!")
+            print(f"Good job, {name}! You guessed my number in {attempts} guesses!")
             break 
 
 guess_the_number()
