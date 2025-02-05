e8f33040d5180f8ebbaf9b6c0d1a4ce947e0ee84
Alan <alan.university2024@gmail.com>
2025-02-05 23:43:44 +0600

    Classes2

---
+++ Lab_3/Classes/Class2.py
@@ -0,0 +1,22 @@
+class Shape:
+    def __init__(self):
+        pass
+
+    def area(self):
+        return 0
+
+class Square(Shape):
+    def __init__(self, length):
+        self.length = length
+
+    def area(self):
+        return self.length * self.length
+
+
+length = float(input())
+
+
+square = Square(length)
+
+
+print(square.area())
