90cb6e51559d92353cf723dc3387d41c2e6e58f1
Alan <alan.university2024@gmail.com>
2025-02-14 12:14:53 +0600

    Generator2 and 3

--- Lab_4/Gen3.py
+++ Lab_4/Gen3.py
@@ -1,10 +1,13 @@
 def Generator(number):
     for numb in range(number + 1):
-        if numb % 2 == 0:
+        if numb % 3 == 0 and numb % 4 == 0:
             yield numb
-
 
 number = int(input())
 result = Generator(number)
 
-print(",".join(str(num) for num in Generator(number)))
+while True:
+    try:
+        print(next(result))
+    except StopIteration:
+        break
