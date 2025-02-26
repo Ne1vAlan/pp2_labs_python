152ca9ba436e1fee685c0bd224d1dbf72069610e
Alan <alan.university2024@gmail.com>
2025-02-26 18:59:58 +0600

    Regex7

---
+++ Lab_5/regex7.py
@@ -0,0 +1,11 @@
+import re
+
+pattern = r"_(.)"
+
+def match(a):
+    camel = re.sub(pattern, lambda match: match.group(1).upper(), text)
+    print(camel[0].lower()+camel[1:])
+    
+text = "Davai suda svoi string"
+match(text)
+    
