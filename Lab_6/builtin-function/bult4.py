2f6e537e35bb7c0be9af6c39ee8727afee647254
Alan <alan.university2024@gmail.com>
2025-03-01 00:34:42 +0600

    built4

---
+++ Lab_6/builtin-function/bult4.py
@@ -0,0 +1,11 @@
+import time
+import math
+
+num = int(input())
+delay = int(input())
+
+time.sleep(delay / 1000)  
+
+result = math.sqrt(num)
+
+print(f"Square root of {num} after {delay} miliseconds is {result}")
